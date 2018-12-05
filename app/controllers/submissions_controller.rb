0e306ff5bd80bbacc9ae25b0118a0d6efcfc10a0
luisvalerio <luisvh18@gmail.com>
2018-12-04 21:01:04 -0600

    Added View and controller method for handling questions

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -16,11 +16,19 @@ class SubmissionsController < ApplicationController
 
         if params[:submission_id] # Working with an ongoing assessment
             survey_submission = SurveySubmission.find(params[:submission_id])
+            @survey_submission = params[:submission_id]
+            @survey_template_id = survey_submission.survey_template_id
+            @questions_count = survey_submission.survey_template.questions.count
+            byebug
             # TODO update new answer
         elsif params[:email].match(valid_email_regex) # No assessment in process, create one (SurveySubmission)
-            SurveySubmission.create(
-                    name: params[:email],
+            @survey_template_id = params[:survey_id]
+            @questions_count = SurveyTemplate.find(params[:survey_id]).questions.count
+            @question_index = 0
+            survey_submission = SurveySubmission.create(
+                    email: params[:email],
                     survey_template_id: params[:survey_id])
+            @survey_submission = survey_submission
         else # Invalid email
                 flash[:error] = "Please enter a valid email"
                 redirect_to begin_survey_url(:id => params[:survey_id])
